1364cb29c30dc3f486c2700250f1fb79e49e9ec5
Jomar Pueyo <jomarpueyo@gmail.com>
2018-10-17 15:27:47 -0500

    Create isPalindrome.java
    Problem 9. Palidrome Number https://leetcode.com/problems/palindrome-number/description/

---
+++ isPalindrome.java
@@ -0,0 +1,16 @@
+//Problem 9. Palidrome Number https://leetcode.com/problems/palindrome-number/description/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        char [] xArray = (""+x).toCharArray();
+        for(int i=0; i<xArray.length/2;i++){
+            if(xArray[i]!=xArray[xArray.length-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
