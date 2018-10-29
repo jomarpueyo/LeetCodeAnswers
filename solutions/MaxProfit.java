70a90abecea081008c1271dc376143769c66c30c
Jomar Pueyo <jomarpueyo@gmail.com>
2018-10-29 15:27:01 -0500

    Create MaxProfit.java

---
+++ solutions/MaxProfit.java
@@ -0,0 +1,12 @@
+//122. Best Time to Buy and Sell Stock II //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for(int i=0; i<prices.length-1; i++){
+            if(prices[i]<prices[i+1]){
+                profit+=prices[i+1]-prices[i];
+            }
+        }
+        return profit;        
+    }
+}
