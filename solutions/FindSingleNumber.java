41e82e9c89e4d292af90d62eee674f04171d8fa9
Jomar Pueyo <jomarpueyo@gmail.com>
2018-10-29 04:50:02 -0500

    Update FindSingleNumber.java

--- solutions/FindSingleNumber.java
+++ solutions/FindSingleNumber.java
@@ -1,14 +1,24 @@
 //136. Single Number https://leetcode.com/problems/single-number/description/
 class FindSingleNumber {
+//List Method
+//     public int singleNumber(int[] nums) {
+//         List<Integer> list = new ArrayList<>();
+//         for(int num : nums){
+//             if(list.contains(num)){
+//                 list.remove(new Integer(num));
+//             }else{
+//                 list.add(num);
+//             }
+//         }
+//         return list.get(0);
+//     }
+
+//XorAns method
     public int singleNumber(int[] nums) {
-        List<Integer> list = new ArrayList<>();
+        int xorAns = 0;
         for(int num : nums){
-            if(list.contains(num)){
-                list.remove(new Integer(num));
-            }else{
-                list.add(num);
-            }
+            xorAns ^= num;
         }
-        return list.get(0);
+        return xorAns;
     }
 }
