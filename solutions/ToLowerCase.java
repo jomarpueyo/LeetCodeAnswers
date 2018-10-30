1c5fe89dcabb5830f719dcb7fdf2141bc585009b
Jomar Pueyo <jomarpueyo@gmail.com>
2018-10-30 17:58:26 -0500

    Create ToLowerCase.java

---
+++ solutions/ToLowerCase.java
@@ -0,0 +1,6 @@
+//709. To Lower Case https://leetcode.com/problems/to-lower-case/description/
+class ToLowerCase {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
